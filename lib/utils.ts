3e0f583a9e254f903c83ba34a67811e09eaf4135
Leandro Ruel <proflrr@hotmail.com>
2025-03-11 15:05:14 -0300

    add missing utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,25 @@ import { twMerge } from "tailwind-merge"
 export function cx(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const focusRing = "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2";
+
+export const focusInput = "focus:border-primary focus:ring-1 focus:ring-primary focus:outline-none";
+
+export const hasErrorInput = "border-red-500 ring-1 ring-red-500 text-red-600 placeholder:text-red-400 focus:border-red-500 focus:ring-red-500";
+
+export const formatters = {
+  default: (value: any) => String(value),
+  currency: (value: number) => {
+    return new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    }).format(value);
+  },
+  date: (value: string | Date) => {
+    return new Intl.DateTimeFormat('pt-BR').format(new Date(value));
+  },
+  number: (value: number) => {
+    return new Intl.NumberFormat('pt-BR').format(value);
+  }
+};
